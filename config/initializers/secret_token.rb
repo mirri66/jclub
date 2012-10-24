2bcf86b7e7fae13a52e760a226b7aee269d61340
Grace Tang <tsmgrace@gmail.com>
2012-10-24 01:10:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journalclub::Application.config.secret_token = '43c8278128415e4d59febcb6f3aeabcdc097a15444df9ed1e1011abb9e5af7f46fca3c854264bd596aa2a7856ed7ecb0b3cffd580e2f672f14d3d8cb111e79bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Journalclub::Application.config.session_store :cookie_store, key: '_journalclub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Journalclub::Application.config.session_store :active_record_store
